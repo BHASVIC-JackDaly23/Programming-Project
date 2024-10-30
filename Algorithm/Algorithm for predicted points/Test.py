a8753b12c370b3733db7c2c4a8ef7bf5cc47e09a
Jack <jack.daly23@students.bhasvic.ac.uk>
2024-10-30 19:30:13 +0000

    Base for the API

--- Algorithm/Algorithm for predicted points/Test.py
+++ Algorithm/Algorithm for predicted points/Test.py
@@ -1,5 +1,19 @@
 import requests
+import http.client
 
-response = requests.get("https://dashboard.api-football.com/soccer/requests")
-print(response.status_code)
+conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+headers = {
+    'x-rapidapi-host': "v3.football.api-sports.io",
+    'x-rapidapi-key': "05163fc687b34571a2f4aae19f980f33"
+    }
+
+conn.request("GET", "/players?id=200&season=2022", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
+
+
 
